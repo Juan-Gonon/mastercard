cebca52a4e2398769d510890d2a07289308dbf49
Juan Gonon <juanfegonon27@gmail.com>
2024-04-05 11:30:07 -0600

    event bubling

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,10 +1,18 @@
 const EventItem = ({info, id, name, image, onEventClick})=>{
+
+    const handleSeeMoreClick = (e)=>{
+        e.stopPropagation();
+        onEventClick(id);
+    }
+
+
     return (
-        <div>
+
+        <div onClick={()=> console.log('Padre click')}>
             <img src={image} alt={name} width={200}/>
             <h4>{name}</h4>
             <p>{info}</p>
-            <button onClick={()=> onEventClick(id)} >Ver mas</button>
+            <button onClick={handleSeeMoreClick} >Ver mas</button>
 
         </div>
     )
