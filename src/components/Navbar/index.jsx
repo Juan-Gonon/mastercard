eaa2209a165f50580715752303473d7f4d2888fa
Juan Gonon <juanfegonon27@gmail.com>
2024-04-05 11:00:04 -0600

    events

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,23 @@
-
+import { useState } from "react";
 
 const Navbar = ()=>{
+
+    const [search, setSearch] = useState('');
+
+    const handleInputChange = (e)=>{
+        console.log(e.target.value);
+        setSearch(e.target.value)
+    }
+
+    console.log(search)
 
     return(
         <div>
             <p>Mi boletará</p>
-            <input placeholder="Busca tu evento favorito"/>
+            <input placeholder="Busca tu evento favorito" 
+            onChange={handleInputChange}
+            value={search}
+            />
         </div>
     );
 
