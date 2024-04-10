b53538d9f00abe5442180884d06ea5bd6fed1a46
Juan Gonon <juanfegonon27@gmail.com>
2024-04-10 10:11:59 -0600

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-import { useState, useEffect, useRef } from 'react';
 import Routes from './routes';
-
-import Navbar from './components/Navbar/index';
-import Events from './components/Events';
-import SignupForm from './components/SignupForm';
-
 import './App.css';
 
 
 
 function App() {
-/*
-  const [searchTerm, setSearchTerm] = useState('');
-  const containerRef = useRef();
 
-
-  useEffect(()=>{
-    console.log('Hola soy el searchTerm');
-  }, [searchTerm]);
-
-  
-  const handleNavbarSearch = (term)=>{
-    console.log(containerRef.current)
-    setSearchTerm(term)
-
-  }
-
-
-  return (
-    <>
-      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-      <Events searchValue={searchTerm}/>
-
-      
-    </>
-  )
-  */
 
   return <Routes/>
 }
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,4 +1,36 @@
-const Home = ()=> <div className="home">Home</div>
+import { useState, useEffect, useRef } from 'react';
+import Navbar from '../../components/Navbar';
+import Events from '../../components/Events'
+// import SignupForm from './components/SignupForm';
+
+const Home = ()=> {
+    
+    const [searchTerm, setSearchTerm] = useState('');
+    const containerRef = useRef();
+
+
+    useEffect(()=>{
+        console.log('Hola soy el searchTerm');
+    }, [searchTerm]);
+
+    
+    const handleNavbarSearch = (term)=>{
+        console.log(containerRef.current)
+        setSearchTerm(term)
+
+    }
+
+
+    return (
+        <>
+        <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
+        <Events searchValue={searchTerm}/>
+
+        
+        </>
+    )
+  
+}
 
 
 export default Home;
