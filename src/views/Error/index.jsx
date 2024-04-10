24d3c7bb0bcbba559f43a0dd9e27b031ee18f12b
Juan Gonon <juanfegonon27@gmail.com>
2024-04-10 10:30:46 -0600

    useRouteError

--- src/views/Error/index.jsx
+++ src/views/Error/index.jsx
@@ -1,10 +1,16 @@
-import style from './Error404.module.css'
+import {useRouteError} from "react-router-dom"
+import style from './Error404.module.css';
 
 const Error404 = ()=>{
+    const error = useRouteError();
+
+ 
+
+
     return(
         <div className={style.container}>
-            <h3 className={style.title}>Ops!</h3>
-            <p className={style.description}>No hemos encontrado la ruta que buscas</p>
+            <h3 className={style.title}>Ops! {error.status}</h3>
+            <p className={style.description}>{error.data}</p>
         </div>
     )
 }
