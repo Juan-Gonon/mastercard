10c75f190086ef60bbb6490ec7be63a51c714250
Juan Gonon <juanfegonon27@gmail.com>
2024-04-10 11:26:46 -0600

    useParams

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,4 +1,14 @@
-const Detail = ()=> <div className="details">Detail</div>
+import {useParams} from "react-router-dom";
+
+const Detail = ()=> {
+
+    const {eventId} = useParams();
+
+    console.log(eventId)
+
+
+    return <div className="details">Detail</div>
+}
 
 
 export default Detail
