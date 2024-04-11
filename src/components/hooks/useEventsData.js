e13bac184951bf982327b6b9b39a5dcf9449fadc
Juan Gonon <juanfegonon27@gmail.com>
2024-04-11 09:42:54 -0600

    fetch

--- src/components/hooks/useEventsData.js
+++ src/components/hooks/useEventsData.js
@@ -4,23 +4,33 @@ import eventsData from '../../data/events.json';
 
 const useEventsData = ()=>{
     const [data, setData] = useState([]);
-    // const {_embedded : {events}} = data.current;
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState();
 
     useEffect(()=>{
-        //load API call
-        setTimeout(()=>{
 
+        const fetchEvents = async ()=>{
             try{
-                setData(eventsData);
-                setIsLoading(false);
-                // console.log(data)
-            }catch(e){
-                setError(error)
+                const response = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=J2HCSxqn9fAoV9fUHdB0zYlplrSJGxkU&countryCode=MX");
+
+                if(response.status != 200){
+                    throw new Error('Failed to fetch events. Status: ' + response.status);
+                    
+                }else{
+                    const data = await response.json();
+                    setData(data)
+                    setIsLoading(false)
+
+                }
+
+            }catch(error){
+                console.error('Error fetching events:', error);
+                setError(error);
             }
-           
-        }, 4000)
+        }
+
+        fetchEvents();
+
     }, [])
 
 
