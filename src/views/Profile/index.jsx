106bdd0a5e667998b5426517323b7bdf76ab18f0
Juan Gonon <juanfegonon27@gmail.com>
2024-04-15 10:25:30 -0600

    crando tabs

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,10 +1,34 @@
-import {Outlet} from "react-router-dom"
+import {Outlet, useLocation, useNavigate } from "react-router-dom";
+import style from './profile.module.css'
+
 
 
 const Profile = ()=>{
+
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+
+    const handleTabClick = (path)=>{
+        navigate(`/profile/${path}`)
+    };
+
+    console.log(pathname)
     return (
         <div className="profile">
-            Profile
+            <div>
+                <span 
+                className={pathname.includes('my-info') ? style.active : ''}
+                onClick={()=> handleTabClick('my-info')} 
+                >
+                    My información 
+                </span>
+                <span 
+                className={pathname.includes('liked-events') ? style.active : ''}
+                onClick={()=> handleTabClick('liked-events')} 
+                >
+                    Eventos Favoritos
+                </span>
+            </div>
             <Outlet></Outlet>
         </div>
     )
