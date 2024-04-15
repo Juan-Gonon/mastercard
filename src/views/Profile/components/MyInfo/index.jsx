d3b28071570b1f474f304e353d1e596077cf7bf2
Juan Gonon <juanfegonon27@gmail.com>
2024-04-15 11:00:41 -0600

    myInfo form

--- src/views/Profile/components/MyInfo/index.jsx
+++ src/views/Profile/components/MyInfo/index.jsx
@@ -1,7 +1,35 @@
+import {useForm} from 'react-hook-form';
+import style from './MyInfo.module.css'
+
+
 const MyInfo = ()=>{
+    const {handleSubmit, register} = useForm();
+
+    const handleFormSubmit = (data)=>{
+        console.log(data)
+    }
 
     return (
-        <div>Mi Info</div>
+        <form onSubmit={handleSubmit(handleFormSubmit)} className={style.form}>
+            <label htmlFor="" className={style.label}>
+                Name
+                <input className={style.input} type="text" {...register('name', {required: true})} />
+            </label >
+   
+            <label htmlFor="" className={style.label}>
+                Email
+                <input className={style.input} type="text" {...register('email', {required: true})} />
+            </label>
+        
+            <label htmlFor="" className={style.label}>
+                Edad
+                <input className={style.input} type="text" {...register('age', {required: true})} />
+            </label>
+
+            <button type="submit" className={style.submitButton} >Save</button>
+
+
+        </form>
     )
 
 
