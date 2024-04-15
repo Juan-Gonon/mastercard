e55c1666fe2dfcee9797cbc0eadce0c0df735557
Juan Gonon <juanfegonon27@gmail.com>
2024-04-15 11:31:37 -0600

    profile liked

--- src/views/Profile/components/LikedEvents/index.jsx
+++ src/views/Profile/components/LikedEvents/index.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+
+import { LIKED_EVENTS_STORAGE_KEY } from "../../../../utils/constants";
+
+
 const likedEvents = ()=>{
+
+
+    useEffect(()=>{
+        const fetchEventDetails = async ()=>{
+            try {
+                // const response = await fetch(
+                //     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${import.meta.env.VITE_TICKETMASTER_API_KEY}&countryCode=MX${params?.length ? params : ''}`
+                //   );
+
+                const likeEvents = JSON.parse(localStorage.getItem(LIKED_EVENTS_STORAGE_KEY)) || [];
+                  
+                  console.log(likeEvents)
+            } catch (error) {
+                
+            }
+        }
+
+        fetchEventDetails();
+    }, [])
 
     return (
         <div>Like events</div>
