62b72cd2c4614c6755756bc096c8461c2878d905
Juan Gonon <juanfegonon27@gmail.com>
2024-04-15 11:47:09 -0600

    create results

--- src/views/Profile/components/LikedEvents/index.jsx
+++ src/views/Profile/components/LikedEvents/index.jsx
@@ -1,28 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { LIKED_EVENTS_STORAGE_KEY } from "../../../../utils/constants";
 
 
 const likedEvents = ()=>{
 
+    const [events, setEvents] = useState([]);
+    const [isLoading, seIsLoading] = useState(false);
+    const [error, setError] = useState({})
+
 
     useEffect(()=>{
         const fetchEventDetails = async ()=>{
             try {
-                // const response = await fetch(
-                //     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${import.meta.env.VITE_TICKETMASTER_API_KEY}&countryCode=MX${params?.length ? params : ''}`
-                //   );
-
+                seIsLoading(true);
+              
                 const likeEvents = JSON.parse(localStorage.getItem(LIKED_EVENTS_STORAGE_KEY)) || [];
                   
                   console.log(likeEvents)
+
+                  const results = [];
+
+
+                  for (const eventId of likeEvents){
+                    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=${import.meta.env.VITE_TICKETMASTER_API_KEY}`);
+
+                   
+
+                    const data = await response.json()
+
+                    console.log(data)
+
+                    results.push(data);
+
+                  }
+
+                  setEvents(results);
+
+
+
             } catch (error) {
-                
+                setError(error)
+            } finally{
+                seIsLoading(false);
             }
         }
 
         fetchEventDetails();
     }, [])
+
+    if(Object.keys(error).length > 0){
+        return <div>Ha ocurrido un error</div>
+    }
+    if(isLoading){
+        return <div>Cargando...</div>
+    }
 
     return (
         <div>Like events</div>
