cace6a7388a7494c7e5249ec1d9cdc06ba5f806f
Juan Gonon <juanfegonon27@gmail.com>
2024-04-15 11:09:26 -0600

    required form

--- src/views/Profile/components/MyInfo/index.jsx
+++ src/views/Profile/components/MyInfo/index.jsx
@@ -3,27 +3,28 @@ import style from './MyInfo.module.css'
 
 
 const MyInfo = ()=>{
-    const {handleSubmit, register} = useForm();
+    const {handleSubmit, register, formState:{errors}} = useForm();
 
     const handleFormSubmit = (data)=>{
         console.log(data)
     }
 
+
     return (
         <form onSubmit={handleSubmit(handleFormSubmit)} className={style.form}>
             <label htmlFor="" className={style.label}>
                 Name
-                <input className={style.input} type="text" {...register('name', {required: true})} />
+                <input className={style.input} type="text" {...register('name', {required: true, minLength:1, maxLength:120})} />
             </label >
    
             <label htmlFor="" className={style.label}>
                 Email
-                <input className={style.input} type="text" {...register('email', {required: true})} />
+                <input className={style.input} type="text" {...register('email', {required: true, minLength:1, maxLength: 120})} />
             </label>
         
             <label htmlFor="" className={style.label}>
                 Edad
-                <input className={style.input} type="text" {...register('age', {required: true})} />
+                <input className={style.input} type="number" {...register('age', {required: true, min : 1, max: 120, valueAsNumber:true})} />
             </label>
 
             <button type="submit" className={style.submitButton} >Save</button>
